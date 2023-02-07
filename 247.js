ef69921c1edef45ddd04f2decdb23b572a803878
Vovka67 <114399957+Vovka67@users.noreply.github.com>
2023-02-07 17:12:12 +0500

    Add files via upload

---
+++ 247.js
@@ -0,0 +1,53 @@
+'use strict'
+// 1
+// let elem = document.querySelector('#elem');
+// let elem1 = document.querySelector('#elem1');
+// elem1.addEventListener('click', func1);
+// function func1() {
+// 	console.log(elem.textContent);
+// }
+
+// 2
+// let elem = document.querySelector('#elem');
+// let elem1 = document.querySelector('#elem1');
+// elem1.addEventListener('click', func1);
+// function func1() {
+// 	console.log(elem.textContent);
+//     elem.textContent = 'не молчи вова';
+// }
+
+// 3
+// let elem1 = document.querySelector('#elem1');
+// let elem2 = document.querySelector('#elem2');
+// let elem3 = document.querySelector('#elem3');
+// elem3.addEventListener('click', func1);
+// function func1() {
+// 	console.log(Number(elem1.textContent) + Number(elem2.textContent));
+// }
+
+// 4
+// let num1 = document.querySelector('#num1');
+// let num2 = document.querySelector('#num2');
+// let num3 = document.querySelector('#num3');
+// let elem = document.querySelector('#elem');
+// let div = document.querySelector('#div');
+// elem.addEventListener('click', function() {
+// 	console.log(div = Number(num1.textContent) + Number(num2.textContent) + Number(num3.textContent));
+// });
+
+
+// 5
+// let num = document.querySelector('#num');
+// let elem = document.querySelector('#elem');
+// elem.addEventListener('click', func1);
+// function func1() {
+// 	console.log(Number(num.textContent) + 1);
+// 	console.log(Number(num.textContent));
+// }
+
+// 6
+// let text = document.querySelector('#text');
+// let elem = document.querySelector('#elem');
+// elem.addEventListener('click', function() {
+// 	console.log(text.textContent + '!!!');
+// });
---
+++ 248.js
@@ -0,0 +1,13 @@
+'use strict'
+// let text = document.querySelector('#text');
+// let elem = document.querySelector('#elem');
+// elem.addEventListener('click', function() {
+// 	console.log(text.innerHTML);
+// });
+
+// let text = document.querySelector('#text');
+// let elem = document.querySelector('#elem');
+// elem.addEventListener('click', function() {
+// 	console.log(text.innerHTML);
+//     text.innerHTML = '<b>Пока!</b>';
+// });
